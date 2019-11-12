292af88ef7edb71e5b077917aa3b1186080a2d04
vaishali p <narayanvaishali@giantgroup.com>
2019-11-12 13:17:18 +0000

    Added StaffId to Login

--- supportupdateAPI/Models/EmployeeLoginType.cs
+++ supportupdateAPI/Models/EmployeeLoginType.cs
@@ -19,6 +19,8 @@ namespace supportupdateAPI.Models
         public string Password { get; set; }
         public string EmployeeName { get; set; }
 
+        public int StaffID { get; set; }
+
     }
 
 }
